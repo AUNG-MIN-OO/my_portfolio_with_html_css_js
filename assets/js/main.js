04af727a4e6d5c8a6198a59ac7083f65cdde1d03
Aung Min Oo <aungminoo1314@gmail.com>
2021-08-11 12:00:08 +0900

    portfolio completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,4 +35,32 @@ typewriter
     .pauseFor(1000)
     .start();
 
+//owlcarousel
+
+const responsive  ={
+    0:{
+        items:1
+    },
+    320:{
+        items:1
+    },
+    560:{
+        items: 2
+    },
+    960:{
+        items:1
+    },
+}
+
+$('.owl-carousel').owlCarousel({
+    dots: false,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout: 2000,
+    nav: true,
+    navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+    responsive: responsive
+});
+
+
 
